839ac8310ebae8dbfd8270aeb8af57e1ab97d797
Daniel Rodriguez <111673881+Dr0d1536@users.noreply.github.com>
2023-04-01 20:20:54 -0700

    Add files via upload
    Second attempt

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, render_template
+import sqlalchemy as sql
+
+app=Flask(__name__)
+engine=sql.create_engine('sqlite:///data/db.sqlite')
+
+
+# Points the to the directions of the data source
+@app.route('/data')
+def return_data(): 
+    results=engine.execute('select * from data').all()
+    social_df=[]
+    for each_result in results: 
+        create_poloy=eval(each_result[0])
+        social_df.append(create_poloy)
+    return jsonify(social_df)
+
+
+# Call's upon the the HTLM file
+@app.route('/')
+def home(): 
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ logic.js
@@ -0,0 +1,25 @@
+d3.json('http://localhost:5000/data').then(function(data){
+	
+// Starting point. The center of California
+	var myMap=L.map("map", {
+	    center: [37.166111, -119.449444],
+	    zoom: 20,
+	})
+
+//grabs out tiles
+	var streetMap=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+	streetMap.addTo(myMap);
+
+//iterates tbrough our data to create the poloygons
+	for (let i=0; i<data.length; i++){
+		let data_block=data[i][0];
+		let change_order_of_cords=[];
+		for (let j=0; j<data_block.length; j++){
+			let coordinate=data_block[j];
+			change_order_of_cords.push([coordinate[1], coordinate[0]]);
+		};
+
+//Adds the ploygons to the map. 
+		L.polygon(change_order_of_cords, {color: 'blue'}).addTo(myMap);
+	};
+});
